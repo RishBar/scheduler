17806f2b18ed58d1efdc9e0d90c1a44ac671308e
Rishav Baral <rishavbaral6@gmail.com>
2020-07-17 20:02:21 -0700

    add mode comments and cleant up code

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -77,13 +77,11 @@ describe("Application", () => {
     expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
   });
   it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
-    // 1. Render the Application.
+
     const { container } = render(<Application />);
 
-    // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
-    // 3. Click the "Delete" button on the booked appointment.
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
